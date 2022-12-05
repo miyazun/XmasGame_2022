b754d3bd14518a6c36d83fe90115c392a28c6525
miyazun <g2054795@gmail.com>
2022-12-06 02:34:32 +0900

    下の街並みが動くようにしました

---
+++ Assets/Scripts/BuildingsMove.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingsMove : MonoBehaviour
+{
+    private Transform trees;
+    private Rigidbody2D rb;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float back;
+    private Vector3 startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trees = this.gameObject.transform;
+        rb = GetComponent<Rigidbody2D>();
+        startPosition = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector3(-speed, 0.0f, 0.0f);
+        if (trees.position.x <= back)
+        {
+            trees.position = startPosition;
+        }
+    }
+}
