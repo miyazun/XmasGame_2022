b4c9c42332bc84def7988f1e9c5fa2eb67d5a674
miyazun <g2054795@gmail.com>
2022-12-24 17:22:21 +0900

    midianimationtrackの追加にあたり、曲の変更。midiファイルの追加

--- Assets/Scripts/NotesMove.cs
+++ Assets/Scripts/NotesMove.cs
@@ -5,16 +5,24 @@ public class NotesMove : MonoBehaviour
 {
     [SerializeField]
     private float speed;
+    [SerializeField]
+    private float over;
 
     // Start is called before the first frame update
     void Start()
     {
         var xMove = Observable.EveryUpdate().Subscribe(_ => XMove()).AddTo(this);
+        var destroyObj = Observable.EveryUpdate().Where(_ => transform.position.x <= over).Subscribe(_ => Delete()).AddTo(this);
     }
 
     private void XMove()
     {
         transform.Translate(-speed * Time.deltaTime, 0, 0);
+    }
+
+    public void Delete()
+    {
+        Destroy(this.gameObject);
     }
 
 }
