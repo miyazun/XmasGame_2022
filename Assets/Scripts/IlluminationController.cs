98cc80b3d6d1ecfbcd9bd01568c306548094fb0e
miyazun <g2054795@gmail.com>
2022-12-30 14:52:40 +0900

    イルミネーションを追加。時間ごとにイメージが変更

---
+++ Assets/Scripts/IlluminationController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UniRx;
+
+public class IlluminationController : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite[] illumination;
+    [SerializeField]
+    private SpriteRenderer spriteRenderer;
+    private int num;
+
+    void Start()
+    {
+        num = 0;
+        var changeIllumination = Observable
+                                .EveryUpdate()
+                                .ThrottleFirst(TimeSpan.FromSeconds(1))
+                                .Subscribe(_ => ChangeImage());
+    }
+
+    private void ChangeImage()
+    {
+        num++;
+        num = num % illumination.Length;
+        spriteRenderer.sprite = illumination[num];
+    }
+}
